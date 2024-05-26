0bdf82eb90bad8654aca1eefeb32a58b14601f84
Asela S Perera <136217850+aselasperera@users.noreply.github.com>
2024-05-26 11:07:31 +0530

    Db

---
+++ database/db.go
@@ -0,0 +1,20 @@
+// database/db.go
+package database
+
+import (
+	"github.com/aselasperera/go-fiber-crud-api/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	var err error
+	DB, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB.AutoMigrate(&models.User{})
+}
