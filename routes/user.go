7e3172f9c664c8205a25b554273a822a1c2907c5
Asela S Perera <136217850+aselasperera@users.noreply.github.com>
2024-05-26 11:10:51 +0530

    routes

---
+++ routes/user.go
@@ -0,0 +1,15 @@
+// routes/user.go
+package routes
+
+import (
+	"github.com/aselasperera/go-fiber-crud-api/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupUserRoutes(app *fiber.App) {
+	app.Get("/users", handlers.GetUsers)
+	app.Get("/users/:id", handlers.GetUser)
+	app.Post("/users", handlers.CreateUser)
+	app.Put("/users/:id", handlers.UpdateUser)
+	app.Delete("/users/:id", handlers.DeleteUser)
+}
